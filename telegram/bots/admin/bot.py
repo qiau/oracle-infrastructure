3e6d9d18920db67c7373efeb00ecd49b0e94c1e0
qiau <raihanekap04@gmail.com>
2026-08-22 23:46:21 +0700

    fix telegram folder

--- telegram/bots/admin/bot.py
+++ telegram/bots/admin/bot.py
@@ -1,7 +1,5 @@
 from telegram.ext import ApplicationBuilder
-
 from config.settings import ADMIN_BOT_TOKEN
-
 from bots.admin.commands import register_commands
 
 
@@ -9,17 +7,17 @@ def create_app():
     app = (
         ApplicationBuilder()
         .token(ADMIN_BOT_TOKEN)
+        .base_url("http://telegram-bot-api:8081/bot")
+        .base_file_url("http://telegram-bot-api:8081/file/bot")
+        .local_mode(True)
         .build()
     )
-
     register_commands(app)
-
     return app
 
 
 def start():
     app = create_app()
-
     app.run_polling(
         allowed_updates=[
             "message",
